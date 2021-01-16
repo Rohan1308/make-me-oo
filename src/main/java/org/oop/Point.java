3b0e669c4a800e8359f4c7c563c7287ff8319619
Rohan Kumar <rokuog@gmail.com>
2021-01-16 12:33:00 +0530

    Refactor Point class to include distance functionality

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,27 +1,23 @@
 package org.oop;
 
+import static java.lang.Math.sqrt;
+
 public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double distanceFrom(Point p) {
+        double xDiff = p.x - this.x;
+        double yDiff = p.y - this.y;
+        return sqrt(square(xDiff) + square(yDiff));
+    }
+
+    private double square(double n) {
+        return Math.pow(n, 2);
     }
 }
---
+++ src/test/java/org/oop/PointTest.java
@@ -0,0 +1,46 @@
+package org.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class PointTest {
+
+    @Test
+    void twoPointsWithSameXAndYCoordinatesShouldHaveADistanceOfZero() {
+        Point origin = new Point(0, 0);
+
+        double distance = origin.distanceFrom(origin);
+
+        assertEquals(0, distance);
+    }
+
+    @Test
+    void distanceBetweenOriginAndPointsOnUnitCircleShouldBeOne() {
+        Point origin = new Point(0, 0);
+        Point point1 = new Point(1, 0);
+
+        double distance1 = origin.distanceFrom(point1);
+
+        assertEquals(1, distance1);
+
+
+        Point point2 = new Point(0, 1);
+
+        double distance2 = origin.distanceFrom(point2);
+
+        assertEquals(1, distance2);
+    }
+
+    @Test
+    void distanceBetweenTwoOppositePointsOnUnitCircleShouldBeTwo() {
+        Point point1 = new Point(1, 0);
+        Point point2 = new Point(-1, 0);
+
+        double distance = point1.distanceFrom(point2);
+
+        assertEquals(2, distance);
+    }
+    
+}
