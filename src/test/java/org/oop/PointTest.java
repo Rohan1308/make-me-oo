72ee5880d2e8ceb10d59ad95e1a584a3c7cd4f5b
Rohan Kumar <rokuog@gmail.com>
2021-01-16 12:51:20 +0530

    Refactor tests to use AAA format

--- src/test/java/org/oop/PointTest.java
+++ src/test/java/org/oop/PointTest.java
@@ -47,29 +47,50 @@ public class PointTest {
     void originAndPointOnPostiveXAxisShouldBeZeroRadiansAway() {
         Point origin = new Point(0, 0);
         Point point1 = new Point(1, 0);
+
+        double direction1 = origin.direction(point1);
+
+        assertEquals(0, direction1);
+
+
         Point point2 = new Point(3, 0);
 
-        assertEquals(0, origin.direction(point1));
-        assertEquals(0, origin.direction(point2));
+        double direction2 = origin.direction(point2);
+
+        assertEquals(0, direction2);
     }
 
     @Test
     void originAndPointOnNegativeXAxisShouldBePiRadiansAway() {
         Point origin = new Point(0, 0);
         Point point1 = new Point(-1, 0);
+
+        double direction1 = origin.direction((point1));
+
+        assertEquals(Math.PI, direction1);
+
+
         Point point2 = new Point(-3, 0);
 
-        assertEquals(Math.PI, origin.direction((point1)));
-        assertEquals(Math.PI, origin.direction((point2)));
+        double direction2 = origin.direction((point2));
+
+        assertEquals(Math.PI, direction2);
     }
 
     @Test
     void originAndPointOnYAxisShouldBeHalfPiRadiansAway() {
         Point origin = new Point(0, 0);
         Point point1 = new Point(0, 1);
+
+        double direction1 = origin.direction(point1);
+
+        assertEquals(Math.PI / 2, direction1);
+
+
         Point point2 = new Point(0, 3);
 
-        assertEquals(Math.PI / 2, origin.direction(point1));
-        assertEquals(Math.PI / 2, origin.direction(point2));
+        double direction2 = origin.direction(point2);
+
+        assertEquals(Math.PI / 2, direction2);
     }
 }
